02fd952d553a038c88d45879d5f0b39d7892ee47
jeffwscott <jeffscott@outlook.com>
2020-09-25 17:09:52 -0400

    updated contract names

--- frontend/src/js/config.js
+++ frontend/src/js/config.js
@@ -1,11 +1,11 @@
 export const config = {
     totalPixels: 256,
-    infoContract: "con_pf_test",
-    masterContract: "con_pf_master_test",
+    infoContract: "con_pixel_frames",
+    masterContract: "con_pixel_frames_master",
     currencySymbol: "dTau",
     domainName: "https://pixelframes.lamden.io",
     //blockExplorer: "http://localhost:1337",
-    blockExplorer: "https://explorer.lamden.io/api",
+    blockExplorer: "https://testnet.lamden.io/api",
     //blockExplorer: "http://localhost:1337",
     masternode: "https://testnet-master-1.lamden.io"
 }
