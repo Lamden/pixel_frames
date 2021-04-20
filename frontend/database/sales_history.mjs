29f4b0933980636211a821fd0dbcb430af385af4
jeffwscott <jeffscott@outlook.com>
2021-04-20 09:19:51 -0400

    query sales history

---
+++ frontend/database/sales_history.mjs
@@ -0,0 +1,19 @@
+import { getDatabase, mongoose_models} from "../database/database.mjs";
+
+var myArgs = process.argv.slice(2);
+var uid = myArgs[0]
+
+const sales_history = () => {
+    const process = async () => {
+        let db = await getDatabase()
+        let models = mongoose_models
+
+        let things = await models.SalesHistory.find({uid})
+        console.log(things)
+    }
+
+    return { process }
+}
+
+let salesHistory = sales_history()
+salesHistory.process().then(() => process.exit(1))
