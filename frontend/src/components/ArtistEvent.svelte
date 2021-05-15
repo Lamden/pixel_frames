0265dd2b3febec3b9c51196be4b1a2d2ff711cbe
jeffwscott <jeffscott@outlook.com>
2021-05-15 15:01:25 -0400

    update date on pic

--- frontend/src/components/ArtistEvent.svelte
+++ frontend/src/components/ArtistEvent.svelte
@@ -77,8 +77,12 @@
 		</h2>
 		<h3><strong>{eventInfo.name}</strong> by <a href="{`./creator/${eventInfo.artistVk}`}">{eventInfo.artistName}</a></h3>
 		<img src="{`/img/events/${eventInfo.image}`}" alt="event announcement" />
-		<p>Buy these collectables starting on {new Date(eventInfo.startDate).toLocaleString()} (your local time)</p>
-		<p class="subtitle">$250 USD per NFT, prices reduced by $50 every hour until the collection is SOLD!</p>
+		{#if eventStarted}
+			<p>ON SALE NOW!</p>
+		{:else}
+			<p>Buy these collectables starting on {new Date(eventInfo.startDate).toLocaleString()} (your local time)</p>
+		{/if}
+		<p class="subtitle">Prices reduced by 500 TAU every hour until the collection is SOLD!</p>
 		<p>ALL WHALES WELCOME!</p>
 		<div class="gallery">
 			<PixelWall mostLiked={eventInfo.artList} />
