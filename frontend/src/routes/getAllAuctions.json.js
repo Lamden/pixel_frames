389058bfca6b4379c2d1542a42fa22cddcaaa667
JeffWScott <jeffscott@outlook.com>
2021-07-24 10:24:30 -0400

    get the fucking auctions

--- frontend/src/routes/getAllAuctions.json.js
+++ frontend/src/routes/getAllAuctions.json.js
@@ -1,6 +1,6 @@
 export async function get(req, res) {
 	console.log("HERE")
-	let auctions = await global.db.models.AuctionHistory.find({})
+	let auctions = await global.db.queries.getAllAuctions()
 	console.log({auctions})
 
 	res.setHeader('Content-Type', 'application/json');
