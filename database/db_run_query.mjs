9cb7ebc77e4d2669eed07d200724eb71e260daf2
JeffWScott <jeffscott@outlook.com>
2021-07-24 10:54:44 -0400

    redundant code

--- database/db_run_query.mjs
+++ database/db_run_query.mjs
@@ -1,22 +1,19 @@
 import {config} from 'dotenv'
 config()
 
-import {getQueries} from './db_queries.mjs'
 import { getDatabase} from "./database.mjs"
 
 const runQuery = async (queryName) => {
     const db = await getDatabase()
-    console.log(db)
-    const queries = getQueries(db)
 
     // Validate Args
-    const availableQueries = Object.keys(queries)
+    const availableQueries = Object.keys(db.queries)
 
      if (!availableQueries.includes(queryName)) {
         throw new Error(`Query doesn't exists. Queries available are: ${availableQueries.join(", ")}`)
     }
 
-     let results =  await queries[queryName]()
+     let results =  await db.queries[queryName]()
     console.log({results})
     process.exit(1)
 }
